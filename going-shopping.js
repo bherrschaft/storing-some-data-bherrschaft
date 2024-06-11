8a03427bd661b096491f0c3401a78a3fd731b12a
bherrschaft <bherrschaft92994@gmail.com>
2024-06-10 21:13:18 -0400

    shoppinggg

---
+++ going-shopping.js
@@ -0,0 +1,30 @@
+
+let shoppingList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
+
+
+shoppingList.push('fruit loops');
+
+
+shoppingList[shoppingList.indexOf('coffee')] = 'fair trade coffee';
+
+
+shoppingList.splice(shoppingList.indexOf('chips'), 2, 'rice', 'beans');
+
+
+let shoppingCart = [];
+
+
+shoppingCart.push(shoppingList.pop());
+
+
+shoppingCart.push(shoppingList.shift());
+
+
+while (shoppingList.length > 0) {
+  shoppingCart.push(shoppingList.pop());
+}
+
+
+shoppingCart.sort().reverse();
+
+console.log(shoppingCart.join(', '));
