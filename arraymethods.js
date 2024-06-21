98c028668d89f55dd7ea3df0376cc25934ba2642
bherrschaft <bherrschaft92994@gmail.com>
2024-06-21 08:15:25 -0400

    Update arraymethods.js

--- arraymethods.js
+++ arraymethods.js
@@ -1,3 +1,4 @@
+// An array of user objects, each with an id, first name, last name, number of logins, and whether they are a premium member.
 const users = [
     {id: 1232, firstName: 'Cam', lastName: 'Danvers', logins: 35, isPremiumMember: false},
     {id: 3283, firstName: 'Elijah', lastName: 'Hawkings', logins: 3, isPremiumMember: true},
@@ -9,24 +10,29 @@ const users = [
     {id: 3879, firstName: 'Ira', lastName: 'Bolislovitz', logins: 9, isPremiumMember: false}
 ];
 
-
+// Finding the user whose first name is 'Jose'.
 const userJose = users.find(user => user.firstName === 'Jose');
+// Logging the user object for 'Jose' to the console.
 console.log(userJose);
 
-
+// Filtering out users who are premium members.
 const premiumMembers = users.filter(user => user.isPremiumMember);
+// Logging the array of premium members to the console.
 console.log(premiumMembers);
 
-
+// Mapping the array of users to an array of their last names.
 const userLastNames = users.map(user => user.lastName);
+// Logging the array of last names to the console.
 console.log(userLastNames);
 
-
+// Filtering out users who have logged in more than 10 times and mapping them to a string with their first and last names.
 const activeUsers = users
     .filter(user => user.logins > 10)
     .map(user => `${user.firstName} ${user.lastName}`);
+// Logging the array of active users' names to the console.
 console.log(activeUsers);
 
-
+// Reducing the array of users to the total number of logins.
 const totalLogins = users.reduce((sum, user) => sum + user.logins, 0);
+// Logging the total number of logins to the console.
 console.log(totalLogins);
